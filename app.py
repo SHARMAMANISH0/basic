0e01a4e5494a22c03ceab170306dfb99f493d662
SHARMAMANISH0 <139530006+SHARMAMANISH0@users.noreply.github.com>
2024-09-30 13:19:01 +0530

    first commit

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/my-page')
+def my_page():
+    return render_template('my_page1.html')
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
